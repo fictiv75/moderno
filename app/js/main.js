cd3c81012812bef9c3414262038689e7c7237eb2
Pavel <fictiv75@gmail.com>
2020-09-11 23:06:10 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,37 @@
-$(function(){
-
+$(function () {
   $(".rate-star").rateYo({
     rating: 5,
     readOnly: true,
-    starWidth: "12px"
+    starWidth: "12px",
   });
 
-  $('.product-slider__inner').slick({
+  $(".product-slider__inner").slick({
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     arrows: false,
-
   });
 
-  var mixer = mixitup('.products__inner-box');
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$",
+  });
 
-  
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").addClass('active');
+    $(".icon-th-large").removeClass('active');
+  });
+
+  $(".icon-th-large").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th-large").addClass('active');
+    $(".icon-th-list").removeClass('active');
+  });
+
+  var mixer = mixitup(".products__inner-box");
 });
